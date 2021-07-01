e10a4c7a9e035b3ad392b79287e829e82a8cb8b9
Lara Mestnik <lara.mestnik@gmail.com>
2021-07-01 20:53:33 +0200

    welcome title

--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import welcomeBackground from '../assets/welcomebg.jpg';
+import Button from '../components/Button';
 
 console.log(welcomeBackground);
 
 export default function Welcome() {
     return (
-        <main id="welcomeBg" style={{ backgroundImage: `url(${welcomeBackground})` }}>
+        <main className="welcome welcome--bg" style={{ backgroundImage: `linear-gradient(rgba(5, 25, 32, .9), rgba(5, 25, 32, 0.2)), url(${welcomeBackground})` }}>
+            <h1 className="welcome__title"><span className="welcome__title__subtitle">Your personal</span> movies assistant</h1>
+            <Button btnStyle="primary" btnText="Sign Up" />
         </main>
+
     )
 }
