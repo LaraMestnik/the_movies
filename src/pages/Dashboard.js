e5e5e11ad76f2e3b0a585092cad6a2d589066471
Lara Mestnik <lara.mestnik@gmail.com>
2021-10-04 13:02:59 +0200

    log user out

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from 'react';
+import { auth } from '../firebase';
+import { useHistory } from 'react-router-dom';
 
 export default function Dashboard() {
+    const history = useHistory();
+
+    function handleLogOut(e) {
+        e.preventDefault();
+        auth.signOut()
+        history.push("/register");
+    }
+
     return (
         <div>
             <h1>Dashboard</h1>
+            <button onClick={handleLogOut}>Log Out</button>
         </div>
     )
 }
